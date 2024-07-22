b76c1a31e95394be39f0b90379c73344e0e2be21
Dmitry Fadeev <ge86yew@mytum.de>
2024-07-22 21:44:30 +0200

    added layout and some routes

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,7 @@
-import { Inter } from 'next/font/google';
+import {Inter} from 'next/font/google';
 
-export const inter = Inter({ subsets: ['latin'] });
+export const inter = Inter({subsets: ['latin']});
+export const lusitana = {
+    className: 'lusitana-font-class', // Replace with the actual class name or object
+    // Other properties if any
+};
