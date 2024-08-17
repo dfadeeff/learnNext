7cc9c17ecb400d426986bbe7f5d3f7ffa11a01d1
Dmitry Fadeev <ge86yew@mytum.de>
2024-08-17 13:35:57 +0200

    added invoices

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,7 +6,9 @@ import {fetchRevenue, fetchLatestInvoices} from '@/app/lib/data';
 
 export default async function Page() {
     const revenue = await fetchRevenue();
+    console.log('Fetched revenue:', revenue);
     const latestInvoices = await fetchLatestInvoices();
+    console.log('Fetched latest invoices:', latestInvoices);
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
