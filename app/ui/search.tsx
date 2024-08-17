706a380b0858800822f8a88d3752db85639f45c8
Dmitry Fadeev <ge86yew@mytum.de>
2024-08-17 18:49:45 +0200

    added debouncing

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,14 +2,16 @@
 
 import {MagnifyingGlassIcon} from '@heroicons/react/24/outline';
 import {useSearchParams, usePathname, useRouter} from 'next/navigation';
+import {useDebouncedCallback} from 'use-debounce';
 
 export default function Search({placeholder}: { placeholder: string }) {
     const searchParams = useSearchParams();
     const pathname = usePathname();
     const {replace} = useRouter();
 
+    const handleSearch = useDebouncedCallback((term: string) => {
+        console.log(`Searching... ${term}`);
 
-    function handleSearch(term: string) {
         const params = new URLSearchParams(searchParams);
         if (term) {
             params.set('query', term);
@@ -17,8 +19,7 @@ export default function Search({placeholder}: { placeholder: string }) {
             params.delete('query');
         }
         replace(`${pathname}?${params.toString()}`);
-        console.log(term);
-    }
+    }, 300);
 
     return (
         <div className="relative flex flex-1 flex-shrink-0">
@@ -31,10 +32,11 @@ export default function Search({placeholder}: { placeholder: string }) {
                 onChange={(e) => {
                     handleSearch(e.target.value);
                 }}
-                defaultValue={searchParams.get('query')?.toString()}
+                defaultValue={searchParams.get('query')?.toString() || ''}
             />
             <MagnifyingGlassIcon
-                className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900"/>
+                className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900"
+            />
         </div>
     );
 }
